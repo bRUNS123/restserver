9e06da5e50ce9232b82a67aa8b0228d7e27ed65b
bRUNS123 <101034235+bRUNS123@users.noreply.github.com>
2022-05-28 23:55:19 -0400

    index

---
+++ public/components/css.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="temple-class">
+    <img 
+        class="temple-class__img"
+        src="">
+        <p>Titulo</p>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Template'
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
--- public/indexvue.js
+++ public/indexvue.js
@@ -9,33 +9,68 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'BONENODE',
-        activo: true,
+        title: 'NodeBone',
         full_name: '',
-        validation_error: ''
+        validation_error: '',
+        codeUser: '',
+        text_error: false,
+        text_success: false,
+        main: true,
+        registro: false,
+        respuesta: false,
+
     },
     methods: {
-        update:function () {
-            this.activo = false;
-                     console.log('probando funcion');
-                     console.log(this.activo);
-                     
+        updateMain:function () {
+       this.main = !this.main
         },
+
+        updateRespuesta:function () {
+            this.respuesta = !this.respuesta
+             },
+    
+        
+        updateRegistro:function () {
+            this.registro = !this.registro
+            console.log(this.registro)
+             },
+    
         verificar: function(){
-            if (Object.keys(this.full_name).length < 4 ) {
-                this.validation_error = 'El codigo no es valido'
-                setInterval(this.desactivate, 3000)
+            if (Object.keys(this.full_name).length < 6 && this.validation_error != 'CODIGO123') {                  
+                setTimeout(() => {
+                 this.updateMain()
+                 this.updateRespuesta()
+                }, 2000);       
+                this.validation_error = 'El codigo no es valido';  
+                this.text_error= true
+                this.text_success = false
+               this.updateMain()
+               this.updateRespuesta()
+                
+            
 
             } else {
+                setTimeout(() => {
+                    this.updateRespuesta()  
+                    this.updateRegistro()                 
+                                                              
+                   }, 3000);       
+                   
+
                 this.validation_error = 'Gracias.'
+                this.text_success = true
+                this.text_error= false
+                this.updateMain()
+                this.updateRespuesta()
+               
+             
+           
+                
+                
 
             }
 
         },
-        desactivate: function(){
-            this.activo = true;
-        }
-
            
     },
 //     template: `
