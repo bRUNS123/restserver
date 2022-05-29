9136b5b5d9cfb9297315934aa09eeaa97b4d3ca4
bRUNS123 <101034235+bRUNS123@users.noreply.github.com>
2022-05-28 20:27:18 -0400

    index

--- public/indexvue.js
+++ public/indexvue.js
@@ -1,10 +1,48 @@
+// Vue.component('index', {
+//     template: `
+//     <div>
+//     <h2>{{title}}</h2>
+//     </div>
+//     `
+// })
+
 new Vue({
     el: '#app',
     data: {
         title: 'BONENODE',
         activo: true,
-        texto: ''
-    }
-    
-});
+        full_name: '',
+        validation_error: ''
+    },
+    methods: {
+        update:function () {
+            this.activo = false;
+                     console.log('probando funcion');
+                     console.log(this.activo);
+                     
+        },
+        verificar: function(){
+            if (Object.keys(this.full_name).length < 4 ) {
+                this.validation_error = 'El nombre no es valido'
+                setInterval(this.desactivate, 3000)
 
+            } else {
+                this.validation_error = 'Gracias.'
+
+            }
+
+        },
+        desactivate: function(){
+            this.activo = true;
+        }
+
+           
+    },
+//     template: `
+    
+
+// `,
+    
+})
+
+       
